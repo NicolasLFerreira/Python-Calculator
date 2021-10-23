1f43881c85d0ac5fe88eca7172a0a9e64ef88d37
Nicolas Ferreira <nicolas.l.ferreira@gmail.com>
2021-10-24 12:37:58 +1300

    removed junk

--- Python/CalcOlator.py
+++ Python/CalcOlator.py
@@ -9,28 +9,13 @@ from enum import *
 # Top level widget
 root = Tk()
 
-class A(IntEnum):
-    x = 1
-    y = 2
-    z = 3
 
 # Main function. Assembles the code together
 def main():
-    #a = []
-    #a.append(1)
-    #a.append(2)
-    
-    #string = ""
-    #for x in a:
-    #    string += str(x)
-
-    #print(str(float(string)))
 
     calc = Calculation()
     app = Application(None, calc, root)
     app.master.title("Calc-o-Lator")
     app.mainloop()
-
-    #print(1 == A.y.real)
 
 main()
