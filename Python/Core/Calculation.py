3826535e566810b11abb6e8ae93f932689eddb19
Nicolas Ferreira <nicolas.l.ferreira@gmail.com>
2021-11-02 20:41:02 +1300

    Code documentation.

--- Python/Core/Calculation.py
+++ Python/Core/Calculation.py
@@ -16,33 +16,44 @@ from math import *
 # - Radiation #
 class Calculation:
 	""" Everything related to calculation is inside here """
+	
+	# Constructor.
 	def __init__(self):
-		# Stored value from previous calculations
+
+		# Stored value from previous calculations.
 		self.result = 0.0
-		# Value currently stored for calculation
+
+		# Value currently stored for calculation.
 		self.operating_value = 0.0
-		# Number being currently input by the user
+
+		# Number being currently input by the user.
 		self.building_number = ['']
-		# If the current number is decimal or not
+
+		# If the current number is decimal or not.
 		self.decimal = False
-		# Switch for radiation operations
+
+		# Switch for radiation operations.
 		self.rad_operation = False
 
-		# Enum of the operation currently stored
+		# Enum of the operation currently stored.
 		self.current_operation = None
-		# Whether to allow the input of an operation
+
+		# Whether to allow the input of an operation.
 		self.allow_operation = False
-		# Whether it's the first input of the cycle
+
+		# Whether it's the first input of the cycle.
 		self.first = True
 
+	# Entry method.
+
 	def caller(self, type, id):
-		"""Entry point of the class"""
+		"""Entry point of the class. External methods must call this."""
 
 		output_data = None
 
 		print("call info:", type, id)
 
-		# Calls the respective function for the given input
+		# Calls the respective function for the given input.
 		if (type == Type.NUMBER):
 			print("num")
 			output_data = self.number_call(id)
@@ -53,92 +64,108 @@ class Calculation:
 			print("func")
 			output_data = self.functionality_call(Functionality(id))
 
-		# Output
-		return self.output_update(output_data)
+		# Output.
+		return self.output_handler(output_data)
 	
-	# Input types managers methods
+	# Input types managers methods.
 
 	def number_call(self, id):
-		"""Adds the user input to the self.building_number"""
-
-		print("old info:", self.building_number, self.allow_operation)
-
+		"""Adds the user input to the self.building_number."""
+		
+		# Appends the input to the list.
 		self.building_number.append(str(id))
+
+		# Allows operations to be performed now that there isn't an empty number.
 		self.allow_operation = True
 
-		print("updated info:", self.building_number, self.allow_operation)
-		# Output
+		# Output.
 		return OutputData(is_number = True, first = self.first, current_operation = self.current_operation, building_number = self.building_number, result = self.result)
 		
 
 	def operation_call(self, type):
-		"""Manages operations"""
+		"""Manages operation calls."""
 
-		# Checks to see if it's a operation sign swap or new entry
+		# Checks to see if it's a operation sign swap or new entry.
+		# If it's a new entry, first performs a calculation with the current information.
+
+		# Radiation operations are printed in a special way in the screen.
+		if (type == Operation.RAD):
+			self.rad_operation = True
+		else:
+			self.rad_operation = False
+
 		if (self.allow_operation):
 			self.allow_operation = False
 			self.calculate()
+		
+		# Assigns the new operation type.
 		self.current_operation = type
 
-		# Output
+		# Output.
 		return OutputData(is_number = True, first = self.first, current_operation = self.current_operation, building_number = self.building_number, result = self.result)
 
 	def functionality_call(self, type):
-		"""Calls the respective functionality for the given id"""
+		"""Manages functionality calls."""
+
+		# Performs a calculation and returns outputs the result.
 		if (type == Functionality.EQUALS and self.current_operation != None):
 			return self.equals()
 			print("equals")
 
-		# Resets the entire class
+		# Simply resets the entries.
 		elif (type == Functionality.CLEAR):
 			self.reset()
 
-		# Deletes the last element of the building_number list
+		# Deletes the last element of the building_number list, or performs nothing if it's empty.
 		elif (type == Functionality.DELETE):
 			if (len(self.building_number) > 1):
 				if (self.building_number.pop() == '.'):
 					self.decimal = False
 
-		# Inverts the sign of the building_number
+		# Inverts the sign of the building_number.
 		elif (type == Functionality.INVERT):
 			if (self.building_number[0] == ''):
 				self.building_number[0] = '-'
 			else:
 				self.building_number[0] = ''
 
-		# Makes the building number decimal
+		# Stars the decimal houses in the current number.
 		elif (type == Functionality.DEC):
 			if (not self.decimal):
 				self.building_number.append('.')
 				self.decimal = True
-
-		# Output
+				
+		# Output.
 		return OutputData(is_number = True, first = self.first, rad_operation = self.rad_operation, current_operation = self.current_operation, building_number = self.building_number, result = self.result)
 
-	# Utility methods
+	# Utility methods.
 
 	def equals(self):
-		"""Performs only the calculation of the current expression and returns the result"""
+		"""Performs only the calculation of the current expression and returns the result."""
+
+		# Checks if the building_number is not empty.
 		if (len(self.building_number) > 1):
+
 			self.calculate()
 
-			# Store the output data before it's reseted
+			# Store the output data before it's reseted.
 			output_data = OutputData(is_number = True, first = self.first, result = self.result)
 
-			# Converts the self.result to a list
+			# Converts the self.result to a list.
 			list = self.parse()
+
 			self.reset()
 		
-			# Corrects some of the properties
+			# Changes some of the properties.
 			self.building_number = list
 			self.allow_operation = True
 			self.decimal = True
 
-		# Output
+		# Output.
 		return OutputData(is_number = True, first = self.first, building_number = self.building_number, result = self.result)
 
 	def parse(self):
-		"""Takes the self.result and parses it into a list"""
+		"""Takes the self.result and parses it into a list."""
 		number = str(float(self.result))
 		list_num = ['']
 
@@ -148,7 +175,7 @@ class Calculation:
 		
 
 	def calculate(self):
-		"""Performs the selected operation on both the self.result and self.current_value. self.rad() is a special case"""
+		"""Performs the selected operation on both the self.result and self.current_value. self.rad() is a special case."""
 
 		self.wrapper()
 		if (self.current_operation == Operation.ADD):
@@ -167,14 +194,14 @@ class Calculation:
 			self.result = self.operating_value
 
 	def wrapper(self):
-		"""Converts the building_number value to float, pass it to the operating_value and resets the building_number"""
+		"""Converts the building_number value to float, pass it to the operating_value and resets the building_number."""
 		self.first = False
 		self.operating_value = float("".join(self.building_number))
 		self.building_number = ['']
 		self.decimal = False
 		
 	def reset(self):
-		"""Sets the properties of the object back to the default values"""
+		"""Sets the properties of the object back to the default values."""
 		self.result = 0.0
 		self.operating_value = 0.0
 		self.building_number = ['']
@@ -183,75 +210,93 @@ class Calculation:
 
 		self.current_operation = None
 		self.allow_operation = False
+		self.rad_operation = False
 		self.first = True
 
 		print("reset")
 	
-	# Operations
+	# Operations.
 	
 	def sum(self):
+		"""Summation."""
 		self.result += self.operating_value
 		
 	def sub(self):
+		"""Subtraction."""
 		self.result -= self.operating_value
 		
 	def mult(self):
+		"""Multiplication."""
 		self.result *= self.operating_value
 		
 	def divi(self):
+		"""Division."""
 		if (self.operating_value != 0.0):
 			print(self.operating_value)
 			self.result /= self.operating_value
 	
 	def pow(self):
+		"""Exponentiation."""
 		self.result **= self.operating_value
 		
 	def rad(self):
-		self.rad_operation = True
+		"""Radiation."""
 		self.result **= (1 / self.operating_value)
 
-	# Output
+	# Output.
 
-	def output_update(self, output_data=None):
-		"""Outputs the state of the calculation. Can be a number or error message"""
-
+	def output_handler(self, output_data=None):
+		"""Method for processing the data that will be outputted. Takes an object of the class OutputData and returns something based on it."""
+		
+		# Check to see if the output_data is empty.
 		if (output_data == None):
-			return "ERROR: 01"
+			return "ERROR: empty output data"
 
-		# Numeric output
+		# Number output.
 		if (output_data.is_number):
 			print("output num")
 
-			# Checks the data and prints only the ones that are not empty
-			oper = str(operation_sign()[output_data.current_operation]) if output_data.current_operation != None else None
-			num1 = str(output_data.result)
-			num2 = "".join(output_data.building_number) if output_data.building_number != [] else None
+			# This variable is used in the place of the constant "None". This way I can more
+			# easily change the value being used as the checking value and can debug more easily.
+			bad_value = "bad_value"
 
+			# Checks the data and prints only the ones that are not empty.
+			# Operation sign.
+			oper = str(operation_sign()[output_data.current_operation]) if output_data.current_operation != None else bad_value
+			# Result.
+			num1 = str(output_data.result)
+			# Building number.
+			num2 = str("".join(output_data.building_number)) if output_data.building_number != [''] else bad_value
+
+			# Debugging only.
 			print("oper:", oper)
 			print("num1:", num1)
 			print("num2:", num2)
 
-
+			# Lots of if checks to see what output scenario this is, so to avoid having blank data on the screen.
+			# First time that something is being inputted.
 			if (output_data.first):
-				print("output if 1")
 				return num2
 
-			elif (output_data.rad_operation):
-				print("output if 2")
-				return num2 + " " + oper + " " + num1
+			# Radiation case.
+			if (self.rad_operation):
+					if (num2 == bad_value):
+						return oper + " " + num1
+					else:
+						return num2 + " " + num1
 
-			elif (oper == None and num2 == None):
-				print("output if 3")
+			# Normally after a equals.
+			elif (oper == bad_value and num2 == bad_value):
 				return num1
 
-			elif (oper != None and num2 == None):
-				print("output if 4")
+			# When you only have the result and operation.
+			elif (oper != bad_value and num2 == bad_value):
 				return (str(output_data.result) + " " + oper)
 
-			elif (oper != None and num2 != None and not output_data.first):
-				print("output if 5")
+			# When you have everything.
+			elif (oper != bad_value and num2 != bad_value and not output_data.first):
 				return num1 + " " + oper + " " + num2
 
-		# Error output
+		# Error output.
 		print(">ERROR")
 		return output_data.error
--- Python/Core/OutputData.py
+++ Python/Core/OutputData.py
@@ -1,6 +1,6 @@
 class OutputData():
 	"""Class for holding the necessary data for the input in a way that's easily organizable"""
-	def __init__(self, is_number = True, first = True, rad_operation = False, current_operation = None, building_number = [], result = 0.0, error = "ERROR: 00"):
+	def __init__(self, is_number = True, first = True, rad_operation = False, current_operation = None, building_number = [], result = 0.0, error = "ERROR: unknown error"):
 		self.is_number = is_number
 		self.first = first
 		self.rad_operation = rad_operation
